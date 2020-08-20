7c143dfc33188cc7b1f0213df5d6ed6e5e9a2ca3
shravan9009 <69857094+shravan9009@users.noreply.github.com>
2020-08-20 20:14:15 +0530

    third commit

--- sample_code.sh
+++ sample_code.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-echo "This is an Samples Script from GitHub"
+echo "This is an Samples texting Script from GitHub"
 echo "This is test canada from Linux Server"
 echo "This is hello an example of a script"
 echo "This is running in a build step and it shows changes"
