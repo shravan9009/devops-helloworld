97d305277ff944e4854a502026e330ef914f3be7
shravan9009 <69857094+shravan9009@users.noreply.github.com>
2020-10-20 09:35:02 -0400

    Update sample_code.sh
    1st commit

--- sample_code.sh
+++ sample_code.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
 
-echo "This is an Samples texting Script from GitHub"
+echo "This is an Samples of texting Script from GitHub"
 echo "This is test canada from Linux Server"
-echo "This is hello an example of a script"
+echo "This is hello an example of a  linux script"
 echo "This is running in a build step and it shows changes"
 SUM=$((FIRSTNUMBER + SECONDNUMBER))
 echo "$SUM"
